d65368ab16a37bc5d489c2cfcbfa30fa4247aee5
Keertana7 <keerthanaps2005@gmail.com>
2025-07-29 14:19:38 +0530

    Resolved merge conflict and updated main.py with main branch changes

--- NeuroDrive/main.py
+++ NeuroDrive/main.py
@@ -1,6 +1,8 @@
 import cv2
 import pygame
 import os
+import time
+
 from collections import deque
 
 from detection.face_eye_detection import detect_face_landmarks
@@ -50,20 +52,29 @@ def get_driver_status(eyes_closed, closed_count):
 def main():
     global closed_count, alarm_playing
 
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture('media/sample 1_drowsy.mp4')
 
     if not cap.isOpened():
         logger.error("Camera not accessible.")
         return
+    prev_time = time.time()
+    trigger_start_time = None
+
 
     while True:
         ret, frame = cap.read()
+        start_time = time.time()  # Start time for latency measurement
+        fps = 1.0 / (start_time - prev_time)
+        prev_time = start_time
+
         if not ret:
             logger.warning("Failed to grab frame.")
             break
 
         overlay = frame.copy()
         annotated_frame, left_ear, right_ear, avg_ear, distraction_status = detect_face_landmarks(overlay)
+        latency = (time.time() - start_time) * 1000  # in milliseconds
+
 
         if avg_ear is None:
             status = "No Face Detected"
@@ -78,15 +89,24 @@ def main():
             distraction_status = smooth_pose_status(distraction_status, pose_history)
 
             if status == "Drowsy":
+                if trigger_start_time is None:
+                    trigger_start_time = time.time()  # Start counting when drowsiness begins
                 if not alarm_playing:
                     play_alarm()
                     alarm_playing = True
             else:
+                if trigger_start_time:
+                    time_to_trigger = time.time() - trigger_start_time
+                    logger.info(f"⏱ Time taken to trigger drowsiness: {time_to_trigger:.2f} seconds")
+                    trigger_start_time = None
                 if alarm_playing:
                     stop_alarm()
                     alarm_playing = False
 
+
             logger.info(f"EAR: {avg_ear:.3f}, Status: {status}, Pose: {distraction_status}")
+            logger.info(f"FPS: {fps:.2f}, Latency: {latency:.2f} ms")
+
 
         final_frame = draw_ui_overlay(overlay, status, avg_ear if avg_ear else None, distraction_status)
 
